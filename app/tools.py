cc00134e82de202e4261545004f905640414f564
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-03-13 01:00:29 +0530

    Add multi-agent support with calculator and time tools

--- app/tools.py
+++ app/tools.py
@@ -1,6 +1,24 @@
+from datetime import datetime,timezone
+
 def get_weather(city:str):
     """
     Dummy weather tool.
-    In Production this could call a real weather API.
     """
     return f"The weather in {city} is 30°C and sunny."
+
+def calculate(expression: str):
+    """ 
+    Simple calculator 
+    """
+    try:
+        result = eval(expression)
+        return str(result)
+    except Exception:
+        return "Invalid Calculation!!"
+
+def get_time(city: str):
+    """ 
+    Dummy time tool. 
+    """
+    now = datetime.now(timezone.utc).strftime("%H:%M:%S")
+    return f"The current UTC time is {now} "
